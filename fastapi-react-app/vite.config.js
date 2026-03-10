d34f93593e48c8d7e299a35e57edc325c4883901
Vishal Patel <8295037+vishalp1@users.noreply.github.com>
2026-03-10 16:53:18 -0400

    Updated vite.config.js to fix local build if just using vite
    Goal: Anytime something in this build wants to hit /api, target it at localhost:8000

--- fastapi-react-app/vite.config.js
+++ fastapi-react-app/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+server: {
+  proxy: {
+    // Matches any request starting with /api
+    '/api': {
+      target: 'http://localhost:8000/',
+      changeOrigin: true,
+      secure: false,
+    }
+  }
+},
   plugins: [react()],
 })
